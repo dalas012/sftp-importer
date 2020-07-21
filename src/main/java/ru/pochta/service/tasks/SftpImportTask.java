2a8e548524d731ba1245b3db6eaeff1518d819df
dalay.kuular <dalay.kuular@gmail.com>
2020-07-21 13:56:13 +0300

    Refactoring

--- src/main/java/ru/pochta/service/tasks/SftpImportTask.java
+++ src/main/java/ru/pochta/service/tasks/SftpImportTask.java
@@ -28,13 +28,14 @@ import java.util.stream.Stream;
 @Slf4j
 public class SftpImportTask implements Runnable {
 
-    private YmlProperties properties;
-    private final String CSV_FILE_EXT = ".csv";
-    private final String HASH_SUM_FILE_NAME = "hashsum.txt";
-    private final String ATTRIBUTES_FILE_NAME = "attributes.txt";
-    private final String CHANGED_FILES_DIR_NAME = "changed";
-    private final String RENAMED_FILES_DIR_NAME = "renamed";
-    private final String CURRENT_FILES_DIR_NAME = "current";
+    private static final String CSV_FILE_EXT = ".csv";
+    private static final String HASH_SUM_FILE_NAME = "hashsum.txt";
+    private static final String ATTRIBUTES_FILE_NAME = "attributes.txt";
+    private static final String CHANGED_FILES_DIR_NAME = "changed";
+    private static final String RENAMED_FILES_DIR_NAME = "renamed";
+    private static final String CURRENT_FILES_DIR_NAME = "current";
+
+    private final YmlProperties properties;
     private final String currentFilesPath;
     private final String changedFilesPath;
     private final String renamedFilesPath;
@@ -48,6 +49,9 @@ public class SftpImportTask implements Runnable {
         this.renamedFilesPath = properties.getLocalFilesDirPath() + "/" + RENAMED_FILES_DIR_NAME;
     }
 
+    /**
+     * Main runner
+     */
     @Override
     public void run() {
         try {
@@ -61,6 +65,9 @@ public class SftpImportTask implements Runnable {
         }
     }
 
+    /**
+     * Check local dirs. If there are no dirs then create
+     */
     private void checkLocalDirs() {
         Stream.of(properties.getLocalFilesDirPath(), changedFilesPath, renamedFilesPath, currentFilesPath)
                 .forEach(path -> {
@@ -71,6 +78,13 @@ public class SftpImportTask implements Runnable {
                 });
     }
 
+    /**
+     * Downloads files from sftp. Skip file if:
+     * - the file has not been edited since the last processing
+     * - thi file is uploading into sftp right now
+     *
+     * @throws IOException
+     */
     private void downloadAllFiles() throws IOException {
         File attributesFile = getAttributesFile();
         Set<String> attributesSet = new HashSet<>(Files.readAllLines(attributesFile.toPath()));
@@ -104,6 +118,28 @@ public class SftpImportTask implements Runnable {
         writer.close();
     }
 
+    private File getAttributesFile() throws IOException {
+        File attributesFile = new File(properties.getLocalFilesDirPath() + "/" + ATTRIBUTES_FILE_NAME);
+        if (!attributesFile.exists()) {
+            attributesFile.createNewFile();
+        }
+        return attributesFile;
+    }
+
+    private SSHClient setupSshj() throws IOException {
+        SSHClient client = new SSHClient();
+        client.addHostKeyVerifier(new PromiscuousVerifier());
+        client.connect(properties.getHostname());
+        client.authPassword(properties.getUsername(), properties.getPassword());
+        return client;
+    }
+
+    /**
+     * Check downloaded files by checksum. If checksum has changed
+     * then move the file into /changed dir
+     *
+     * @throws IOException
+     */
     private void checkSums() throws IOException {
         File hashSumsFile = getHashSumsFile();
         Set<String> hashSet = new HashSet<>(Files.readAllLines(hashSumsFile.toPath()));
@@ -134,6 +170,28 @@ public class SftpImportTask implements Runnable {
         writer.close();
     }
 
+    private File getHashSumsFile() throws IOException {
+        File hashSumsFile = new File(properties.getLocalFilesDirPath() + "/" + HASH_SUM_FILE_NAME);
+        if (!hashSumsFile.exists()) {
+            hashSumsFile.createNewFile();
+        }
+        return hashSumsFile;
+    }
+
+    private String getHashSum(Path path) throws NoSuchAlgorithmException, IOException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(Files.readAllBytes(path));
+        byte[] digest = md.digest();
+        return Hex.encodeHexString(digest).toUpperCase();
+    }
+
+    private void moveChangedFile(Path changedFilePath, String hex) throws IOException {
+        Path targetPath = Paths.get(changedFilesPath +
+                "/" + changedFilePath.getFileName().toString().replace(CSV_FILE_EXT, "_" + hex + CSV_FILE_EXT));
+        Files.move(changedFilePath, targetPath);
+        log.info(" - " + targetPath.getFileName() + " - changed");
+    }
+
     private void moveRenamedFile(Path renamedFilePath, String hex) throws IOException {
         Path targetPath = Paths.get(renamedFilesPath +
                 "/" + renamedFilePath.getFileName().toString().replace(CSV_FILE_EXT, "_" + hex + CSV_FILE_EXT));
@@ -146,41 +204,4 @@ public class SftpImportTask implements Runnable {
         }
     }
 
-    private void moveChangedFile(Path changedFilePath, String hex) throws IOException {
-        Path targetPath = Paths.get(changedFilesPath +
-                "/" + changedFilePath.getFileName().toString().replace(CSV_FILE_EXT, "_" + hex + CSV_FILE_EXT));
-        Files.move(changedFilePath, targetPath);
-        log.info(" - " + targetPath.getFileName() + " - changed");
-    }
-
-    private File getHashSumsFile() throws IOException {
-        File hashSumsFile = new File(properties.getLocalFilesDirPath() + "/" + HASH_SUM_FILE_NAME);
-        if (!hashSumsFile.exists()) {
-            hashSumsFile.createNewFile();
-        }
-        return hashSumsFile;
-    }
-
-    private File getAttributesFile() throws IOException {
-        File attributesFile = new File(properties.getLocalFilesDirPath() + "/" + ATTRIBUTES_FILE_NAME);
-        if (!attributesFile.exists()) {
-            attributesFile.createNewFile();
-        }
-        return attributesFile;
-    }
-
-    private String getHashSum(Path path) throws NoSuchAlgorithmException, IOException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(Files.readAllBytes(path));
-        byte[] digest = md.digest();
-        return Hex.encodeHexString(digest).toUpperCase();
-    }
-
-    private SSHClient setupSshj() throws IOException {
-        SSHClient client = new SSHClient();
-        client.addHostKeyVerifier(new PromiscuousVerifier());
-        client.connect(properties.getHostname());
-        client.authPassword(properties.getUsername(), properties.getPassword());
-        return client;
-    }
 }
